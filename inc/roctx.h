69c163bcf97f930010f37f42e0bec38f9196ff50
rachidaChen <31266764+rachidaChen@users.noreply.github.com>
2019-08-16 10:46:10 -0400

    Update roctx.h

--- inc/roctx.h
+++ inc/roctx.h
@@ -27,6 +27,7 @@ THE SOFTWARE.
 #include <iostream>
 #include <string.h>
 #include "roctracer_tx.h"
+#include <stack>
 
 #define PUBLIC_API __attribute__((visibility("default")))
 #if 0
@@ -34,6 +35,7 @@ THE SOFTWARE.
 #define roctxRangeStart      roctxRangeStartA
 #define roctxRangePush       roctxRangePushA
 #endif
+std::stack<std::string> message_stack;
 typedef uint64_t roctxRangeId_t;
 typedef const char* roctxStringHandle_t;
 typedef enum roctxMessageType_t
@@ -41,27 +43,6 @@ typedef enum roctxMessageType_t
     ROCTX_MESSAGE_UNKNOWN          = 0,    
     ROCTX_MESSAGE_TYPE_ASCII       = 1
 } roctxMessageType_t;
-extern "C" {
-
-PUBLIC_API void hsaOpenROCTX();
-PUBLIC_API void hsaCloseROCTX();
-PUBLIC_API void roctxMarkA (roctxStringHandle_t message);
-PUBLIC_API roctxRangeId_t roctxRangePushA(roctxStringHandle_t message);
-PUBLIC_API roctxRangeId_t roctxRangePop() ;
-PUBLIC_API roctxRangeId_t roctxRangeStart(roctxStringHandle_t message);
-PUBLIC_API roctxRangeId_t roctxRangeEnd(roctxRangeId_t rocTxRangeID);
-}
-#if 0
-#define ROCTX_RESOURCE_MAKE_TYPE(CLASS, INDEX) ((((uint32_t)(ROCTX_RESOURCE_CLASS_ ## CLASS))<<16)|((uint32_t)(INDEX)))
-
-typedef enum roctxResourceGenericType_t
-{
-    ROCTX_RESOURCE_TYPE_UNKNOWN = 0,
-    ROCTX_RESOURCE_TYPE_GENERIC_POINTER = ROCTX_RESOURCE_MAKE_TYPE(GENERIC, 1), /**< Generic pointer assumed to have no collisions with other pointers. */
-    ROCTX_RESOURCE_TYPE_GENERIC_THREAD_NATIVE = ROCTX_RESOURCE_MAKE_TYPE(GENERIC, 2), /**< OS native thread identifier. */
-    ROCTX_RESOURCE_TYPE_GENERIC_THREAD_POSIX = ROCTX_RESOURCE_MAKE_TYPE(GENERIC, 3) /**< POSIX pthread identifier. */
-} roctxResourceGenericType_t;
-#endif
 
 enum roctx_api_id_t {
   // block: HSAROCTXAPI API
@@ -136,7 +117,7 @@ uint32_t GetApiCode(const char* str) {
 typedef struct {
   decltype(hsaOpenROCTX)* hsaOpenROCTX_fn;
   decltype(hsaCloseROCTX)* hsaCloseROCTX_fn;
-  decltype(roctxMarkA)* roctxMarkA_fn;
+  //decltype(roctxMarkA)* roctxMarkA_fn;
   decltype(roctxRangePushA)* roctxRangePushA_fn;
   decltype(roctxRangePop)* roctxRangePop_fn;
   decltype(roctxRangeStart)* roctxRangeStart_fn;
@@ -150,8 +131,8 @@ void intercept_ROCTXApiTable(void) {
   HSAROCTXAPI_saved->hsaOpenROCTX_fn = (hsaOpenROCTX_t)dlsym(RTLD_NEXT,"hsaOpenROCTX");
   typedef decltype(HSAROCTXAPI_saved_t::hsaCloseROCTX_fn) hsaCloseROCTX_t;
   HSAROCTXAPI_saved->hsaCloseROCTX_fn = (hsaCloseROCTX_t)dlsym(RTLD_NEXT,"hsaCloseROCTX");
-  typedef decltype(HSAROCTXAPI_saved_t::roctxMarkA_fn) roctxMarkA_t;
-  HSAROCTXAPI_saved->roctxMarkA_fn = (roctxMarkA_t)dlsym(RTLD_NEXT,"roctxMarkA");
+  //typedef decltype(HSAROCTXAPI_saved_t::roctxMarkA_fn) roctxMarkA_t;
+  //HSAROCTXAPI_saved->roctxMarkA_fn = (roctxMarkA_t)dlsym(RTLD_NEXT,"roctxMarkA");
   typedef decltype(HSAROCTXAPI_saved_t::roctxRangePushA_fn) roctxRangePushA_t;
   HSAROCTXAPI_saved->roctxRangePushA_fn = (roctxRangePushA_t)dlsym(RTLD_NEXT,"roctxRangePushA");
   typedef decltype(HSAROCTXAPI_saved_t::roctxRangePop_fn) roctxRangePop_t;
@@ -196,7 +177,8 @@ void roctxMarkA_callback(roctxStringHandle_t message) {
   cb_table.get(ROCTX_API_ID_roctxMarkA, &api_callback_fun, &api_callback_arg);
   api_data.phase = 0;
   if (api_callback_fun) api_callback_fun(ACTIVITY_DOMAIN_ROCTX_API, ROCTX_API_ID_roctxMarkA, &api_data, api_callback_arg);
-  HSAROCTXAPI_saved->roctxMarkA_fn(message);
+  std::cout << "Waiting on " << message << std::endl;
+  //HSAROCTXAPI_saved->roctxMarkA_fn(message);
   api_data.phase = 1;
   if (api_callback_fun) api_callback_fun(ACTIVITY_DOMAIN_ROCTX_API, ROCTX_API_ID_roctxMarkA, &api_data, api_callback_arg);
 }
@@ -208,7 +190,10 @@ void roctxRangePushA_callback(roctxStringHandle_t message) {
   cb_table.get(ROCTX_API_ID_roctxRangePushA, &api_callback_fun, &api_callback_arg);
   api_data.phase = 0;
   if (api_callback_fun) api_callback_fun(ACTIVITY_DOMAIN_ROCTX_API, ROCTX_API_ID_roctxRangePushA, &api_data, api_callback_arg);
-  HSAROCTXAPI_saved->roctxRangePushA_fn(message);
+  // push mmessage onto the stack
+  message_stack.push(message);
+  std::cout << "Current range is " << message_stack.top() << std::endl; 
+  //HSAROCTXAPI_saved->roctxRangePushA_fn(message);
   api_data.phase = 1;
   if (api_callback_fun) api_callback_fun(ACTIVITY_DOMAIN_ROCTX_API, ROCTX_API_ID_roctxRangePushA, &api_data, api_callback_arg);
 }
@@ -219,7 +204,13 @@ void roctxRangePop_callback(void) {
   cb_table.get(ROCTX_API_ID_roctxRangePop, &api_callback_fun, &api_callback_arg);
   api_data.phase = 0;
   if (api_callback_fun) api_callback_fun(ACTIVITY_DOMAIN_ROCTX_API, ROCTX_API_ID_roctxRangePop, &api_data, api_callback_arg);
-  HSAROCTXAPI_saved->roctxRangePop_fn();
+  if (message_stack.empty()) {
+      std::cout << "Stack should not be empty! Did you call roctxRangePop without calling roctxRangePush first ? " << std::endl;
+  } else {
+      std::cout << "End of range " << message_stack.top() << std::endl;
+      message_stack.pop();
+  }
+  //HSAROCTXAPI_saved->roctxRangePop_fn();
   api_data.phase = 1;
   if (api_callback_fun) api_callback_fun(ACTIVITY_DOMAIN_ROCTX_API, ROCTX_API_ID_roctxRangePop, &api_data, api_callback_arg);
 }
