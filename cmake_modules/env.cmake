91684f0aa8f46a2c05e46242e8eb620c35640149
Rachida Kebichi <rachida.kebichi@amd.com>
2019-09-27 17:46:26 -0400

    Adding KFD support.

--- cmake_modules/env.cmake
+++ cmake_modules/env.cmake
@@ -29,6 +29,7 @@ set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fms-extensions" )
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmerge-all-constants" )
 set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC" )
 
+
 set ( CMAKE_SHARED_LINKER_FLAGS "-Wl,-Bdynamic -Wl,-z,noexecstack" )
 
 set ( CMAKE_SKIP_BUILD_RPATH TRUE )
