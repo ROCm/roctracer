3845d4e7b4bd4eae081db7f2b08005e435b1053f
Sam Wu <samwu103@amd.com>
2023-05-11 15:57:41 -0600

    change build flow
    rename . dirs
    
    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,10 +4,22 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import subprocess
+
 from rocm_docs import ROCmDocs
 
-docs_core = ROCmDocs("ROCTracer Documentation")
-docs_core.run_doxygen()
+
+get_name = r'sed -n -e "s/^project(\([A-Za-z-]\+\).*/\1/p" ../CMakeLists.txt'
+get_version = r'sed -n -e "s/^project(.* \([0-9\.]\{1,\}\).*/\1/p" ../CMakeLists.txt'
+name = subprocess.getoutput(get_name)
+version = subprocess.getoutput(get_version)
+if len(version) > 0:
+    name = f"{name} {version} Documentation"
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(name)
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path=".")
 docs_core.setup()
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
